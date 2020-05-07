a935ba2406997ab9ad9145cad4d6ce7521ba42f3
vuvivian <1031355459@qq.com>
2020-05-07 17:46:47 +0800

    创建自定义hook（获取数据的公共hook）

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: vuvivian
  * @Date: 2020-05-07 14:16:50
  * @LastEditors: vuvivian
- * @LastEditTime: 2020-05-07 16:41:05
+ * @LastEditTime: 2020-05-07 17:30:57
  * @Descripttion: 入口文件
  * @FilePath: /covid19-with-react-hooks/src/App.js
  */
@@ -11,41 +11,20 @@ import './App.css';
 import GlobalStats from "./components/GlobalStats";
 import CountriesChart from "./components/CountriesChart";
 import SelectDataKey from "./components/SelectDataKey";
-
-const BASE_URL = "https://corona.lmao.ninja/v2";
+import {useCoronaAPI} from "./hooks/useCoronaAPI";
 
 function App() {
-  const [globalStats, setGlobalStats] = useState({});
   const [key, setKey] = useState('cases');
-  const [countries, setCountries] = useState([])
 
-  // 第二个参数使用空数组可以确保 Effect 只会在组件初次渲染后执行
-  useEffect(() => {
-    const fetchGlobalStats = async () => {
-      const response = await fetch(`${BASE_URL}/all`);
-      const data = await response.json();
-      setGlobalStats(data);
-    };
+  const globalStats = useCoronaAPI("/all", {
+    initialData: {},
+    refetchInterval: 5000
+  });
 
-    // 通过定义了一个 fetchGlobalStats 异步函数并进行调用从而获取数据，而不是直接把这个 async 函数作为 useEffect 的第一个参数；
-    fetchGlobalStats();
-
-    // 每 5 秒重新获取一次
-    const intervalId = setInterval(fetchGlobalStats, 5000);
-
-    // 返回一个函数，用于销毁之前创建的 Interval
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect (() => {
-    const fetchCountries = async () => {
-      const response = await fetch(`${BASE_URL}/countries?sort=${key}`);
-      const data = await response.json();
-      setCountries(data.slice(0, 10));
-      console.log(countries);
-    };
-    fetchCountries();
-  }, [key]);
+  const countries = useCoronaAPI(`/countries?sort=${key}`,{
+    initialData: {},
+    converter: (data) => data.slice(0, 10),
+  })
   
   return (
     <div className="App">
---
+++ src/hooks/useCoronaAPI.jsx
@@ -0,0 +1,41 @@
+/*
+ * @Author: vuvivian
+ * @Date: 2020-05-07 16:55:49
+ * @LastEditors: vuvivian
+ * @LastEditTime: 2020-05-07 17:34:12
+ * @Descripttion: 自定义hook
+ * @FilePath: /covid19-with-react-hooks/src/hooks/useCoronaAPI.jsx
+ */
+import {useState, useEffect, useCallback} from 'react';
+
+const BASE_URL = "https://corona.lmao.ninja/v2";
+
+
+/**
+ * path: API 路径
+ * initialData ：初始为空的默认数据
+ * converter ：对原始数据的转换函数（默认是一个恒等函数）
+ * refetchInterval ：重新获取数据的间隔（以毫秒为单位）
+ */
+export const useCoronaAPI = (path, { initialData = null, converter = (data) => data, refetchInterval = null}) => {
+  const [data, setData] = useState(initialData);
+  // 把 converter 函数用 useCallback 包裹了起来，把记忆化处理后的函数命名为 convertData，并且传入的 deps 参数为空数组 [] ，确保每次渲染都相同。然后把 useEffect 中所有的 converter 函数相应修改成 convertData。
+  const convertData = useCallback(converter, []);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_URL}${path}`);
+      const data  = await response.json();
+      setData(convertData(data));
+    };
+  
+    fetchData();
+  
+    if (refetchInterval) {
+      const intervalId = setInterval(fetchData, refetchInterval);
+      return () => clearInterval(intervalId);
+    }
+  },[convertData, path, refetchInterval]);
+
+  return data;
+}
